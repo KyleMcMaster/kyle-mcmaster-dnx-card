e76750abbce659e3383089dee03c37f0c54aed8c
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2025-12-03 20:40:46 -0500

    Add Bluesky link and emojis

--- KyleMcMaster.Card/Program.cs
+++ KyleMcMaster.Card/Program.cs
@@ -2,19 +2,20 @@
 
 // Basic profile data for Kyle McMaster
 var name = "Kyle McMaster";
-var title = "Software Engineer & .NET Enthusiast";
-var location = "Cleveland, Ohio, USA";
+var title = "Software Engineer & .NET Enthusiast 💻";
+var location = "Cleveland, Ohio, USA 🌆";
 
 // Social links – tweak these as needed
 var website = "https://kylemcmaster.com";
 var github = "https://github.com/kylemcmaster";
 var linkedin = "https://www.linkedin.com/in/kylerobertmcmaster";
 var twitter = "https://x.com/kylemcmaster";
+var bluesky = "https://bsky.app/profile/kylemcmaster.com";
 
 AnsiConsole.Clear();
 
 // Header
-var rule = new Rule($"[bold dodgerblue2]Hi, I'm {name}![/]")
+var rule = new Rule($"[bold dodgerblue2]Hi, I'm {name}! 👋[/]")
 {
 	Style = Style.Parse("grey50")
 };
@@ -25,10 +26,11 @@ AnsiConsole.WriteLine();
 // Create a panel that acts like a business card
 var card = new Panel(
 		new Markup($"[bold white]{title}[/]\n[grey70]{location}[/]\n\n" +
-				   $"[bold yellow]Website[/]: [link={website}]{website}[/]\n" +
-				   $"[bold yellow]GitHub [/]: [link={github}]{github}[/]\n" +
-				   $"[bold yellow]LinkedIn[/]: [link={linkedin}]{linkedin}[/]\n" +
-				   $"[bold yellow]X (Twitter)[/]: [link={twitter}]{twitter}[/]")
+			   $"[bold yellow]Website 🌐[/]: [link={website}]{website}[/]\n" +
+			   $"[bold yellow]GitHub 🧑‍💻[/]: [link={github}]{github}[/]\n" +
+			   $"[bold yellow]LinkedIn 💼[/]: [link={linkedin}]{linkedin}[/]\n" +
+			   $"[bold yellow]X (Twitter) 🐦[/]: [link={twitter}]{twitter}[/]\n" +
+			   $"[bold yellow]Bluesky 🦋[/]: [link={bluesky}]{bluesky}[/]")
 	)
 {
 	Border = BoxBorder.Rounded,
@@ -41,4 +43,4 @@ AnsiConsole.Write(card);
 AnsiConsole.WriteLine();
 
 // A short closing note
-AnsiConsole.MarkupLine("[grey70]Thanks for stopping by! Feel free to reach out on any of the links above.[/]");
+AnsiConsole.MarkupLine("[grey70]Thanks for stopping by! ✨ Feel free to reach out on any of the links above.[/]");
